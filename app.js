9189ee903e2b61bb357e3df3c6e8c6ee7719f2bd
Lochini <luhradik1@gmail.com>
2025-06-23 18:37:45 +0530

    Initial commit: basic CRUD setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const UserController = require('./routes/userRoutes')
+const app = express();
+//inbuilt middleware to pass the data
+app.use(express.json())//JSON Parser -> pass the data
+
+app.use('/api/v1/user/',UserController)
+
+app.listen(5000,()=>console.log('App running on Port 5000'))
+
+//to access user API use below Path
+//http://localhost:5000/api/v1/user (get and post method)
+//http://localhost:5000/api/v1/user/anyid (put and delete method)
